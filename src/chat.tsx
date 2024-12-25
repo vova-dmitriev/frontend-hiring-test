4df39660fd8e3195f92f2d541b869e6b31db225c
Enenlingre <enenlingre@gmail.com>
2024-12-25 23:39:04 +0700

    feat: chat example

---
+++ src/chat.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { ItemContent, Virtuoso } from "react-virtuoso";
+import cn from "clsx";
+import {
+  MessageSender,
+  MessageStatus,
+  type Message,
+} from "../__generated__/resolvers-types";
+import css from "./chat.module.css";
+
+const temp_data: Message[] = Array.from(Array(30), (_, index) => ({
+  id: String(index),
+  text: `Message number ${index}`,
+  status: MessageStatus.Read,
+  updatedAt: new Date().toISOString(),
+  sender: index % 2 ? MessageSender.Admin : MessageSender.Customer,
+}));
+
+const Item: React.FC<Message> = ({ text, sender }) => {
+  return (
+    <div className={css.item}>
+      <div
+        className={cn(
+          css.message,
+          sender === MessageSender.Admin ? css.out : css.in
+        )}
+      >
+        {text}
+      </div>
+    </div>
+  );
+};
+
+const getItem: ItemContent<Message, unknown> = (_, data) => {
+  return <Item {...data} />;
+};
+
+export const Chat: React.FC = () => {
+  return (
+    <div className={css.root}>
+      <div className={css.container}>
+        <Virtuoso className={css.list} data={temp_data} itemContent={getItem} />
+      </div>
+      <div className={css.footer}>
+        <input type="text" className={css.textInput} />
+        <button>Send</button>
+      </div>
+    </div>
+  );
+};
---
+++ src/graphql/client.ts
@@ -0,0 +1,36 @@
+import { ApolloClient, HttpLink, split, InMemoryCache } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { createClient } from "graphql-ws";
+
+const PORT = 4000;
+
+const httpLink = new HttpLink({
+  uri: (operation) =>
+    `http://localhost:${PORT}graphql?op=${operation.operationName}`,
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: `ws://localhost:${PORT}/graphql`,
+  })
+);
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const cache = new InMemoryCache({});
+
+export const client = new ApolloClient({
+  link,
+  cache,
+});
